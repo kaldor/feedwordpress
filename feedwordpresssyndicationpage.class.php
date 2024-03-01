1c2ad2597cdf48270986eaf8d9f6b1f72bfafaab
hughie molloy <hughie.molloy@kaldorgroup.com>
2024-03-01 15:58:44 +0000

    Adding option to force update syndicated items even if modified date unchanged

--- feedwordpresssyndicationpage.class.php
+++ feedwordpresssyndicationpage.class.php
@@ -7,6 +7,7 @@ require_once(dirname(__FILE__) . '/feedfinder.class.php');
 ################################################################################
 
 define('FWP_UPDATE_CHECKED', 'Update Checked');
+define('FWP_FORCE_UPDATE_CHECKED', 'Force Update Checked');
 define('FWP_UNSUB_CHECKED', 'Unsubscribe');
 define('FWP_DELETE_CHECKED', 'Delete');
 define('FWP_RESUB_CHECKED', 'Re-subscribe');
@@ -122,9 +123,14 @@ class FeedWordPressSyndicationPage extends FeedWordPressAdminPage {
 		endif;
 
 		$update_set = array();
+		$update_single_action = false;
+
 		if ($fwp_update_invoke != 'get') :
 			if (is_array(MyPHP::post('link_ids'))
-			and (MyPHP::post('action')==FWP_UPDATE_CHECKED)) :
+			and (
+				(MyPHP::post('action')==FWP_UPDATE_CHECKED)
+				or (MyPHP::post('action')==FWP_FORCE_UPDATE_CHECKED)
+			)) :
 				// Get single link ID or multiple link IDs from REQUEST parameters
 				// if available. Sanitize values for MySQL.
 				$link_list = $this->requested_link_ids_sql();
@@ -149,12 +155,21 @@ class FeedWordPressSyndicationPage extends FeedWordPressAdminPage {
 
 				$targets_keys = array_keys($targets);
 				$first_key = reset($targets_keys);
+
+				$update_single_action = $targets[$first_key];
+
 				if (!is_numeric($first_key)) : // URLs in keys
 					$targets = $targets_keys;
 				endif;
 				$update_set = $targets;
 			endif;
 		endif;
+
+		if (MyPHP::post('action')==FWP_FORCE_UPDATE_CHECKED or $update_single_action === 'Force Update Now'):
+			echo "<p>Force Updating feeds.</p>";
+			add_filter('syndicated_item_force_update', '__return_true');
+		endif;
+
 		return $update_set;
 	}
 
@@ -667,6 +682,7 @@ class FeedWordPressSyndicationPage extends FeedWordPressAdminPage {
 		<input class="button-secondary" type="submit" name="action" value="<?php print FWP_DELETE_CHECKED; ?>" />
 		<?php else : ?>
 		<input class="button-secondary" type="submit" name="action" value="<?php print FWP_UPDATE_CHECKED; ?>" />
+		<input class="button-primary" type="submit" name="action" value="<?php print FWP_FORCE_UPDATE_CHECKED; ?>" style="background:#EE0606" />
 		<input class="button-secondary delete" type="submit" name="action" value="<?php print FWP_UNSUB_CHECKED; ?>" />
 		<?php endif ; ?>
 		</div> <!-- class="alignleft" -->
